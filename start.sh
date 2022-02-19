6fb65bc1a973756bf6eafa8aef4fc68b9f7946ec
beginor <beginor@qq.com>
2022-02-19 14:57:23 +0800

    update startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
-export COCO_MONGO_HOST=mongo
-export COCO_MONGO_ANALYTICS_HOST=mongo
+# export COCO_MONGO_HOST=mongo
+# export COCO_MONGO_ANALYTICS_HOST=mongo
+echo "COCO_MONGO_HOST=$COCO_MONGO_HOST"
+echo "COCO_MONGO_ANALYTICS_HOST=$COCO_MONGO_ANALYTICS_HOST"
+echo "wait for 10 seconds to wait mongo ready."
+sleep 5
+echo "start codecombat"
 npm run start

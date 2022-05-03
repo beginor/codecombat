3954242a80cd244e737f97039c0296e72363a8ef
beginor <beginor@qq.com>
2022-05-03 15:44:51 +0800

    update get-asets

--- get-assets.sh
+++ get-assets.sh
@@ -1,8 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # wget https://codecombat.com
 
-wget https://codecombat.com/file/interface/playback-scrub-start.mp3
 wget https://codecombat.com/file/interface/playback-scrub-end.mp3
 
 mv *.mp3 app/assets/file/interface/ && cp -rf app/assets/file public/
